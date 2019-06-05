8e4b219491e60c1916583aaec83b173e7bc1619f
Felipe Llerena <llerenafelipe7@gmail.com>
2019-06-05 09:26:11 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App container">
+      <header>
+        <h1>React Budget</h1>
+        <h4>Weekly Expense</h4>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
